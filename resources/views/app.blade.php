9292a1f1213e55e9be058b4555fdee5081163c26
spike <you@example.com>
2025-09-13 18:57:46 -0600

    dynamic theminggit status

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,12 +1,13 @@
+@php($theme = config('settings.app.theme.active', 'default'));
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <title>{{ config('app.name') }}</title>
-    @vite('resources/js/Themes/Default/app.js')
-    @inertiaHead
-  </head>
-  <body>
-    @inertia
-  </body>
+<head>
+  <meta charset="utf-8">
+  <title>{{ config('app.name') }}</title>
+  @vite("resources/js/Themes/{$theme}/app.js")
+  @inertiaHead
+</head>
+<body>
+  @inertia
+</body>
 </html>
